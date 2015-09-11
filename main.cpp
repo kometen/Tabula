0e7c66d0eba124de8d15611416629e9848aa6e99
Claus Guttesen <kometen@gmail.com>
2015-09-11 17:43:59 +0200

    Find Content-Length
    In POST atm. Maby in GET later.

--- main.cpp
+++ main.cpp
@@ -10,10 +10,12 @@ using namespace std;
 using namespace boost::asio;
 using namespace boost::asio::ip;
 
+const int BUFSIZE = 4096;
 io_service ioservice;
 tcp::endpoint tcp_endpoint {tcp::v4(), 1967};
 tcp::acceptor tcp_acceptor {ioservice, tcp_endpoint};
-char bytes[4096];
+char bytes[BUFSIZE];
+const string locateContentLength = "Content-Length: ";
 
 void do_read(tcp::socket& tcp_socket, yield_context yield) {
     size_t length = tcp_socket.async_read_some(buffer(bytes), yield);
@@ -22,10 +24,33 @@ void do_read(tcp::socket& tcp_socket, yield_context yield) {
         cout << "GET request" << endl;
     }
     // POST
+    unsigned int cl = 0;
+    string cls = "";
     if (bytes[0] == 'P' && bytes[1] == 'O' && bytes[2] == 'S' && bytes[3] == 'T' && bytes[4] == ' ' && bytes[5] == '/') {
+        for (int j = 0; j < BUFSIZE || bytes[j] == '\0'; ++j) {
+            if (bytes[j] == 'C') {
+                if (bytes[j+8] == 'L') {
+                    if (bytes[j+14] == ':') {
+                        for (int k = (j + 16);; ++k) {
+                            if (bytes[k] == '\r') {
+                                break;
+                            }
+                            cls.push_back(bytes[k]);
+                        }
+                        cl = stoi(cls);
+                        cout << "content-length: " << cl << endl;
+                    }
+                }
+            }
+        }
         cout << endl << "POST request" << endl << endl;
     }
-    cout << "do_read() size: " << length << ", input: " << endl << bytes << endl;
+    cout << "do_read() size: " << length << ", input: " << endl << endl << bytes << endl << endl;
+
+    for (int j = (length - cl); j < length; ++j) {
+        cout << bytes[j];
+    }
+    cout << endl;
 }
 
 void do_write(tcp::socket& tcp_socket, yield_context yield) {
@@ -39,8 +64,6 @@ void do_accept(yield_context yield) {
     for (int i = 0; i < 5; ++i) {
         tcp::socket *tcp_socket = new tcp::socket {ioservice};
         tcp_acceptor.async_accept(*tcp_socket, yield);
-//        spawn(ioservice, [&] (yield_context yieldReadContent) { do_read(*tcp_socket, yieldReadContent); });
-//        spawn(ioservice, [&] (yield_context yieldContent) { do_write(*tcp_socket, yieldContent); });
         spawn(ioservice, [&] (yield_context yield) { do_read(*tcp_socket, yield); });
         spawn(ioservice, [&] (yield_context yield) { do_write(*tcp_socket, yield); });
     }
